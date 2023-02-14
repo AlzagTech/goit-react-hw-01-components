02c5108710b99c2793a11e2062266b197352352d
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-02-14 18:10:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Profile } from "./profile/Profile";
+import { Statistics } from "./statistics/Statisctics";
+import user from '../data/user';
+import data from '../data/data'
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '20vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 16,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      <Profile props={user} />
+      <Statistics title="Upload stats" stats={data}/>
     </div>
   );
 };
